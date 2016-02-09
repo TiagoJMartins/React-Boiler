7edcc3a976acbeb5ba15f8244ebebedb1e87fc6b
Tiago Martins <tiagodksp@gmail.com>
2016-02-09 23:21:04 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# React-Boiler
+
+A simple React boilerplate with hot-loading functionality.
+
+To begin:
+npm install && npm run dev
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    'webpack-dev-server/client?http://127.0.0.1:8080/',
+    'webpack/hot/only-dev-server',
+    './src'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modulesDirectories: ['node_modules', 'src'],
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loaders: ['react-hot', 'babel']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+
+}
